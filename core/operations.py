26c71ede21b975c0d4f21a1ec41912fab1e223ac
caue cristian <caue.dev0@gmail.com>
2026-01-20 14:03:53 -0300

    Criação da função Backspace

--- core/operations.py
+++ core/operations.py
@@ -19,3 +19,42 @@ def insert_text(state: EditorState, value: str) -> EditorState:
         line=line,
         col=col + len(value)
     )
+
+def backspace(state: EditorState) -> EditorState:
+    lines = state.lines
+    line = state.line
+    col = state.col
+
+    if line == 0 and col == 0:
+        return state
+    
+    if col > 0:
+        current_line = lines[line]
+        before = current_line[:col - 1]
+        after = current_line[col:]
+
+        new_line = before + after
+
+        new_lines = lines.copy()
+        new_lines[line] = new_line
+
+        return EditorState(
+            lines=new_lines,
+            line=new_line,
+            col=col -1
+        )
+    
+    prev_line = lines[line - 1]
+    current_line = lines[line]
+
+    merged_line = prev_line + current_line
+
+    new_lines = lines.copy()
+    new_lines[line - 1] = merged_line
+    new_lines.pop(line)
+
+    return EditorState(
+        lines=new_lines,
+        line=new_line,
+        col=len(prev_line)
+    )
