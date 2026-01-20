d54d037f2a401b65acbdad2136f0734f191456e9
caue cristian <caue.dev0@gmail.com>
2026-01-20 14:12:56 -0300

    Criação da função Enter

--- core/operations.py
+++ core/operations.py
@@ -58,3 +58,23 @@ def backspace(state: EditorState) -> EditorState:
         line=new_line,
         col=len(prev_line)
     )
+
+def enter(state: EditorState) -> EditorState:
+    lines = state.lines
+    line = state.line
+    col = state.col
+
+    current_line = lines[line]
+
+    before = current_line[:col]
+    after = current_line[col:]
+
+    new_lines = lines.copy()
+    new_lines[line] = before
+    new_lines.insert(line + 1, after)
+
+    return EditorState(
+        lines=new_lines,
+        line=line + 1,
+        col=0
+    )
